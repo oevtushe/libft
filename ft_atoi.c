4309c7afd6e61721417ca165a8b3789d500eba46
Oleksandr EVTUSHENKO <oevtushe@e2r6p9.unit.ua>
2017-11-07 18:06:10 +0200

    backup atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,37 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/29 16:15:11 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/11/07 13:18:21 by oevtushe         ###   ########.fr       */
+/*   Updated: 2017/11/07 18:04:55 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+static int		is_overflow(int sign, char val, long int cur)
 {
-	int			i;
-	long int	res;
-	int			sign;
+	long int diff;
+
+	diff = LONG_INT_MAX / 10 - cur;
+	if ((val >= '0' && val <= '9' && diff >= 0))
+	{
+		if (diff > 0 || (sign > 0 && ((val - '0' <= LONG_INT_MAX % 10) || \
+					(sign < 0 && val - '0' <= LONG_INT_MAX % 10 + 1))))
+			return (0);
+	}
+	return (1);
+}
+
+int				ft_atoi(const char *str)
+{
+	int					i;
+	long int			res;
+	int					sign;
+	int					len;
 
 	i = 0;
 	sign = 1;
 	res = 0;
+	len = -1;
 	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || \
 			str[i] == '\n' || str[i] == '\f' || str[i] == '\r')
 		++i;
@@ -31,7 +47,10 @@ int		ft_atoi(const char *str)
 	}
 	else if (str[i] == '+')
 		++i;
-	while (str[i] >= '0' && str[i] <= '9')
+	while (++len < 18 && str[i] >= '0' && str[i] <= '9')
 		res = res * 10 + str[i++] - '0';
-	return ((int)((int)sign * res));
+	if (!is_overflow(sign, str[i], res))
+		return ((int)(res * 10 + str[i] - '0') * sign);
+	else
+		return (sign > 0 ? -1 : 0);
 }
