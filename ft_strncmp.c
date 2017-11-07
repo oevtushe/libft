e2d1833883308c231823e5260946e89e849317f4
Oleksandr EVTUSHENKO <oevtushe@e2r6p9.unit.ua>
2017-11-07 19:13:52 +0200

    norms

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/29 15:55:47 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/11/07 13:11:49 by oevtushe         ###   ########.fr       */
+/*   Updated: 2017/11/07 19:02:08 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] != '\0' && (unsigned char)s1[i] == (unsigned char)s2[i] && i < n)
+	while (s1[i] != '\0' && \
+			(unsigned char)s1[i] == (unsigned char)s2[i] && i < n)
 		++i;
 	if (i == n)
 		--i;
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/29 15:32:51 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/11/07 13:10:15 by oevtushe         ###   ########.fr       */
+/*   Updated: 2017/11/07 19:02:33 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	while (haystack[i] != '\0' && i < len)
 	{
 		if (haystack[i] == needle[0])
-			if (i + len_nd <= len && ft_strncmp(&haystack[i], needle, len_nd) == 0)
+			if (i + len_nd <= len \
+					&& ft_strncmp(&haystack[i], needle, len_nd) == 0)
 				return ((char*)&haystack[i]);
 		++i;
 	}
