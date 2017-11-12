b5f3fd5389a3fa1f56e4e1cdd541ce261faf9b31
Oleksandr EVTUSHENKO <oevtushe@e2r6p9.unit.ua>
2017-11-12 18:11:25 +0200

    function to read line from a file

---
+++ ft_freadline.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_freadline.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/12 17:50:41 by oevtushe          #+#    #+#             */
+/*   Updated: 2017/11/12 18:01:48 by oevtushe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void ft_del(void *data, size_t size)
+{
+	size = 0;
+	free((char*)data);
+}
+
+static char	*ft_lsttstr(t_list *lst)
+{
+	int i;
+	int size;
+	char *result;
+
+	i = 0;
+	size = ft_lstlen(lst);
+	result = ft_strnew(size * sizeof(char));
+	while (lst)
+	{
+		result[i++] = *((char*)lst->content);
+		lst = lst->next;
+	}
+	return (result);
+}
+
+char		*ft_freadline(int fd)
+{
+	int i;
+	char c;
+	t_list *lst;
+	char *str;
+
+	lst = NULL;
+	while ((i = read(fd, &c, 1)) > 0 && c != '\n')
+		ft_lstappend(&lst, ft_lstnew(&c, sizeof(char)));
+	str = ft_lsttstr(lst);
+	ft_lstdel(&lst, ft_del);
+	return (str);
+}
