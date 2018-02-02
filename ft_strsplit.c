6b22899d565a9386b54996c6577168796e480886
oevtushe <casio6290@gmail.com>
2018-02-02 16:08:28 +0200

    optimized, logic rebuilded only to arrs

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -5,104 +5,87 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/30 12:20:08 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/11/20 08:45:08 by oevtushe         ###   ########.fr       */
+/*   Created: 2018/01/30 08:48:55 by oevtushe          #+#    #+#             */
+/*   Updated: 2018/01/30 09:48:11 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <libft.h>
 #include <stdlib.h>
 
-static void	char_arr_del(char ***arr, int ln)
+static int		count_blocks(const char *str, char c)
 {
-	while (ln >= 0)
+	int		wc;
+	int		state;
+
+	state = 0;
+	wc = 0;
+	while (*str)
 	{
-		free((*arr)[ln]);
-		ln--;
+		if (*str == c)
+			state = 0;
+		else if (!state)
+		{
+			state = 1;
+			++wc;
+		}
+		++str;
 	}
-	free(*arr);
-	*arr = NULL;
+	return (wc);
 }
 
-static char	**ft_ltsa(t_list *lst)
+static void		clear_arr(char ***wa)
 {
-	char	**arr;
-	size_t	len;
+	int	idx;
+
+	idx = -1;
+	if (wa != NULL)
+	{
+		while ((*wa)[++idx])
+			free((*wa)[idx]);
+		free(*wa);
+		*wa = NULL;
+	}
+}
+
+static int		safety_add(char **wa, int *wa_idx, int end, const char *str)
+{
+	char	*sub;
+
+	sub = ft_strsub(str, 0, end);
+	if (sub)
+	{
+		wa[(*wa_idx)++] = sub;
+	}
+	else
+		clear_arr(&wa);
+	return ((sub) ? 1 : 0);
+}
+
+char		**ft_strsplit(char const *str, char c)
+{
 	int		i;
-	char	*tmp;
+	int		start;
+	int		wa_idx;
+	char	**wa;
 
-	i = 0;
-	len = ft_lstlen(lst);
-	arr = (char**)ft_memalloc(sizeof(char*) * (len + 1));
-	if (arr)
+	i = count_blocks(str, c);
+	wa = (char **)ft_memalloc(sizeof(char *) * (i + 1));
+	if (wa)
 	{
-		while (lst)
-		{
-			tmp = ft_strdup((char*)lst->content);
-			if (!tmp)
+		i = -1;
+		wa_idx = 0;
+		while (str[++i])
+			if (str[i] != c)
 			{
-				char_arr_del(&arr, i);
-				break ;
+				start = i;
+				while (str[i] && str[i] != c)
+					++i;
+				if (!safety_add(wa, &wa_idx, i - start, &str[start]))
+					return ((void*)0);
+				if (!str[i])
+					break ;
 			}
-			arr[i++] = tmp;
-			lst = lst->next;
-		}
 	}
-	return (arr);
-}
-
-static void	ft_mydel(void *cnt, size_t size)
-{
-	size++;
-	free(cnt);
-}
-
-static int	add_word(t_list **lst, char const *s, int begin, int end)
-{
-	char	*tmp;
-	t_list	*ltmp;
-
-	tmp = ft_strsub(s, begin, end);
-	if (!tmp)
-	{
-		ft_lstdel(lst, ft_mydel);
-		return (0);
-	}
-	ltmp = ft_lstnew(tmp, ft_strlen(tmp) + 1);
-	if (!ltmp)
-	{
-		ft_lstdel(lst, ft_mydel);
-		return (0);
-	}
-	ft_lstappend(lst, ltmp);
-	free(tmp);
-	return (1);
-}
-
-char		**ft_strsplit(char const *s, char c)
-{
-	char	**arr;
-	t_list	*lst;
-	int		begin;
-	int		end;
-
-	begin = 0;
-	lst = NULL;
-	arr = NULL;
-	if (s)
-	{
-		while ((end = ft_strfnd(&s[begin], c)) != -1)
-		{
-			if (end)
-				if (!add_word(&lst, s, begin, end))
-					return (NULL);
-			begin += end + 1;
-		}
-		if ((end = ft_strlen(&s[begin])))
-			if (!add_word(&lst, s, begin, end))
-				return (NULL);
-		arr = ft_ltsa(lst);
-		ft_lstdel(&lst, ft_mydel);
-	}
-	return (arr);
+	return (wa);
 }
