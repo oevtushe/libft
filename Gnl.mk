43d5dedcb01de225687d6ee8a818eb3f17274b10
oevtushe <casio6290@gmail.com>
2018-03-08 13:17:56 +0200

    Restructed as addon to main makefile of libft.

--- Gnl.mk
+++ Gnl.mk
@@ -6,22 +6,26 @@
 #    By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/03/07 13:50:55 by oevtushe          #+#    #+#              #
-#    Updated: 2018/03/07 14:44:11 by oevtushe         ###   ########.fr        #
+#    Updated: 2018/03/08 13:10:10 by oevtushe         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-include Makefile
+OBJS_DIR	?= ./
 
-GNL_DIR := gnl
-GNL_DEP := get_next_line.h
-GNL_SRC := get_next_line.c
-GNL_OBJ := $(GNL_SRC:%.c=$(OBJS_DIR)/%.o)
+GNL_DIR		:= gnl
+GNL_DEPF	:= get_next_line.h
+GNL_SRCF 	:= get_next_line.c
 
-gnl: $(NAME) $(GNL_OBJ)
+GNL_DEPS	:= $(addprefix $(GNL_DIR)/,$(GNL_DEPF))
+GNL_SRCS	:= $(addprefix $(GNL_DIR)/,$(GNL_SRCF))
+GNL_OBJS 	:= $(GNL_SRCF:%.c=$(OBJS_DIR)/%.o)
 
-$(GNL_OBJ): $(GNL_DIR)/$(GNL_SRC) $(GNL_DIR)/$(GNL_DEP)
+gnl: $(GNL_OBJS)
+
+$(GNL_OBJS): $(GNL_SRCS) $(GNL_DEPS) $(FT_NAME)
 	@echo "$(BLUE)Adding gnl...$(RESET)"
-	@gcc $(CFLAGS) -o $@ -c $< -I$(GNL_DIR) -I$(DEPS_DIR)
-	@ar -r $(NAME) $(GNL_OBJ)
-	@ranlib $(NAME)
-	@echo "$(YELLOW)Done !$(RESET)"
+	@gcc $(CFLAGS) -o $@ -c $< -I$(GNL_DIR) -I$(FT_DEPS_DIR)
+	@ar -r $(FT_NAME) $(GNL_OBJS)
+	@ranlib $(FT_NAME)
+	@echo "$(CYAN)$(BOLD)Done !$(RESET)"
+.PHONY: gnl
