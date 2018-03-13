74dc4dae340bf0c1766d5912b256aac8c050a00f
oevtushe <casio6290@gmail.com>
2018-03-13 19:06:37 +0200

    Optimization. Use only one call for write.

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,20 +6,20 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 16:32:24 by oevtushe          #+#    #+#             */
-/*   Updated: 2017/11/16 16:16:39 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/03/13 19:00:28 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
 
 void	ft_putstr(char const *s)
 {
-	size_t	i;
+	size_t	len;
 
 	if (s)
 	{
-		i = 0;
-		while (s[i] != '\0')
-			ft_putchar(s[i++]);
+		len = ft_strlen(s);
+		write(1, s, len);
 	}
 }
