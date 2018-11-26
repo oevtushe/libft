b4f040ca7177ed10e565d76e25a0bb9ee33d2da7
oevtushe <casio6290@gmail.com>
2018-11-26 16:20:56 +0200

    Protect ft_strconnect
    Remove case when ft_strconect will remove str, while new
    str wasn't allocated

--- srcs/ft_strconnect.c
+++ srcs/ft_strconnect.c
@@ -6,7 +6,7 @@
 /*   By: oevtushe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/24 14:35:32 by oevtushe          #+#    #+#             */
-/*   Updated: 2018/08/08 18:15:21 by oevtushe         ###   ########.fr       */
+/*   Updated: 2018/11/26 16:20:44 by oevtushe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,26 @@
 
 void		ft_strconnect(char **str1, const char *str2, int side)
 {
+	int		me;
 	char	*res;
 
+	me = 0;
 	res = NULL;
 	if (str1 && str2)
 	{
 		if (!*str1)
+		{
+			me = 1;
 			*str1 = ft_strnew(0);
+		}
 		if (*str1)
 		{
 			if (side > 0)
 				res = ft_strjoin(*str1, str2);
 			else if (side < 0)
 				res = ft_strjoin(str2, *str1);
-			ft_strdel(str1);
+			if (me || res)
+				ft_strdel(str1);
 			if (res)
 				*str1 = res;
 		}
